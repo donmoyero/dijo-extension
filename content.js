2687a91dc4ec7302ee8ec2e4dfd56039fdceacab
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-04-03 04:29:50 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
+const bot = document.createElement("div");
 
+bot.innerText = "🤖 Dijo";
+
+bot.style.position = "fixed";
+bot.style.bottom = "20px";
+bot.style.right = "20px";
+bot.style.background = "#111";
+bot.style.color = "white";
+bot.style.padding = "10px";
+bot.style.borderRadius = "10px";
+bot.style.zIndex = "999999";
+
+document.body.appendChild(bot);
